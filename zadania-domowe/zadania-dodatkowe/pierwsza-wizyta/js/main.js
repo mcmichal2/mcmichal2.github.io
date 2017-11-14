99419cfb4cb5a2d5088d1322c151b80971c87117
mciombor <mcmichal2@gmail.com>
2017-11-14 19:27:24 +0100

    pierwsza wizyta

---
+++ zadania-domowe/zadania-dodatkowe/pierwsza-wizyta/js/main.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function () {
+    addScrolled();
+});
+
+$(document).scroll(function () {
+    addScrolled();
+});
+
+function addScrolled() {
+    if ($(window).width() > 992) {
+        var height = $('#main-nav').height();
+        var scroll = $(window).scrollTop();
+
+        if (scroll > height) {
+            $('#main-nav').addClass('transparent_navbar');
+        } else {
+            $('#main-nav').removeClass('transparent_navbar');
+        }
+    }
+}
