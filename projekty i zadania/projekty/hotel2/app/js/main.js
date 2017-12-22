8f220079ac70ae2bdb5ec41ec9ffbd5a623ef301
Michał Ciombor <31779628+mcmichal2@users.noreply.github.com>
2017-12-22 13:33:50 +0100

    hotel2

---
+++ projekty i zadania/projekty/hotel2/app/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('.bxslider').bxSlider({
+  auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  slideWidth: 600
+    
+    });
+    
+});
+
+        
+       
+                                               
+  
+
+    
+    
